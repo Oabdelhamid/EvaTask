4f1eb61c4186ffbf644d3d5df49c52d2880141a9
Oabdelhamid <100126534+Oabdelhamid@users.noreply.github.com>
2025-05-14 21:02:17 +0300

    CI/CD

--- src/main/java/POM/AdminAfterNewUser.java
+++ src/main/java/POM/AdminAfterNewUser.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class AdminAfterNewUser extends pageObjectModel {
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     public AdminAfterNewUser(WebDriver driver) {
         super(driver);
 
